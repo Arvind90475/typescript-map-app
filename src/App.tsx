c9884ee173da00cbb6805bd6bac557ad846451e7
arvind <arvind.bisht.90475@gmail.com>
2021-08-22 00:26:35 +1000

    map shown on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css';
+import styled from 'styled-components'
+
+const tileurl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tileAttribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const StyledDiv = styled.div`
+  height:100vh;
+  width: 100vw;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledDiv>
+      <MapContainer className='leaflet-map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          url={tileurl}
+          attribution={tileAttribution}
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </StyledDiv>
   );
 }
 
 export default App;
+  
